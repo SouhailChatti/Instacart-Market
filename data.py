8faa7c292a6ccc16f8c349bf05101140a26d9b63
Souhail CHATTI <souhail.chatti@antalis.com>
2020-04-17 02:18:58 +0200

    version 1

---
+++ data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+
+def read_products_data():
+        #load data
+        return pd.read_csv("C:/Users/schatti/Bureau/Instacart Market Basket analyses/Data/products.csv")
+
+def read_orders_data():
+        #load data
+        return pd.read_csv("C:/Users/schatti/Bureau/Instacart Market Basket analyses/Data/orders.csv")
+
+ def read_orderProducts_data():
+        #load data
+        return pd.read_csv("C:/Users/schatti/Bureau/Instacart Market Basket analyses/Data/order_products__prior.csv")
